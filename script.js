6c427dc505d431e24f8a62c11cbd308e598fd52f
guyfoz77 <125807123+guyfoz77@users.noreply.github.com>
2023-04-25 19:07:54 +0100

    Make the equals button work

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const numberButtons = document.querySelectorAll('.num');
-const screen = document.querySelector('.screen')
+//make equals function evaluate what is on the screen (dont use eval function)
+
+const buttons = document.querySelectorAll('.button');
+
+const screen = document.querySelector('.screen');
+const equals = document.querySelector('.equals');
+
 let screenContent = '';
 
-numberButtons.forEach(number => {
-    number.addEventListener('click', function (e) {
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
         screenContent += e.target.dataset.key;
         screen.textContent = screenContent;
     })
 });
+
+equals.addEventListener('click', () => {
+    screenContent = new Function('return '+screenContent)();
+    console.log(screenContent)
+})
+
