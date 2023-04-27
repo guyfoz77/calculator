25e2a50d411a7b46c8e82bd071d6d7d0987ce3e3
guyfoz77 <125807123+guyfoz77@users.noreply.github.com>
2023-04-27 14:07:44 +0100

    Separate screen into sections

--- script.js
+++ script.js
@@ -1,21 +1,57 @@
-//make equals function evaluate what is on the screen (dont use eval function)
 
-const buttons = document.querySelectorAll('.button');
+// Commenting all this out as I feel like its the easy way and not in the spirit of the project.
+// const buttons = document.querySelectorAll('.button');
 
-const screen = document.querySelector('.screen');
-const equals = document.querySelector('.equals');
+// const screen = document.querySelector('.screen');
+// const equals = document.querySelector('.equals');
+
+// let screenContent = '';
+
+// buttons.forEach(button => {
+//     button.addEventListener('click', function (e) {
+//         screenContent += e.target.dataset.key;
+//         screen.textContent = screenContent;
+//     })
+// });
+
+// equals.addEventListener('click', () => {
+//     screenContent = new Function('return '+screenContent)();
+//     console.log(screenContent)
+// })
+
+//get screen number
+//when operator button is pressed, move screen number to variable
+//get new number
+//when operator button is pressed, make the calculation and move answer to the main screen
+
+const numbers = document.querySelectorAll('.num');
+const operatorButtons = document.querySelectorAll('.op')
+
+const topLeftScreen = document.querySelector('.topLeftScreen');
+const topRightScreen = document.querySelector('.topRightScreen');
+const bottomScreen = document.querySelector('.bottomScreen');
+
 
 let screenContent = '';
+let numMemory = ''
 
-buttons.forEach(button => {
-    button.addEventListener('click', function (e) {
+numbers.forEach(number => {
+    number.addEventListener('click', function (e) {
         screenContent += e.target.dataset.key;
-        screen.textContent = screenContent;
+        bottomScreen.textContent = screenContent;
     })
 });
 
-equals.addEventListener('click', () => {
-    screenContent = new Function('return '+screenContent)();
-    console.log(screenContent)
+operatorButtons.forEach(operatorButton => {
+    operatorButton.addEventListener('click', function () {
+        if (numMemory == '') {
+            numMemory = screenContent;
+            screenContent = '';
+            topLeftScreen.textContent = numMemory;
+            bottomScreen.textContent = '';
+        }
+
+    })
 })
 
+// function evaluate(a, b, operator)
